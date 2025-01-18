d57deebef28e0492596488ab84b183af66bd238b
flehmenlips <123845044+flehmenlips@users.noreply.github.com>
2025-01-18 15:12:18 -0800

    fixing VITE error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    commonjsOptions: {
-      include: []
-    }
-  }
-}); 
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], build: { outDir: "dist", sourcemap: true }, server: { port: 3000 } });
